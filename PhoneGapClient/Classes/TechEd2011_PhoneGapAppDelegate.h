963224195056cd8603e92626511f92d5d6ff749c
Simon Guest <me@simonguest.com>
2011-05-18 17:15:34 -0400

    Added PhoneGap client

---
+++ PhoneGapClient/Classes/TechEd2011_PhoneGapAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  TechEd2011_PhoneGapAppDelegate.h
+//  TechEd2011_PhoneGap
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface TechEd2011_PhoneGapAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
